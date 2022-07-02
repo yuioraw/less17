4d4cbd51fdbd7871975f8319cee6c6f502ff2923
andrey <telezhenko.aa@mail.ru>
2022-07-02 22:16:06 +0300

    Initial commit

---
+++ src/test/java/RestTests.java
@@ -0,0 +1,203 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class RestTests {
+
+    @Test
+    void successfulLogin() {
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"cityslicka\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void negativeLogin() {
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void successfulRegister() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unsuccessfulRegister() {
+        String data = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void delayedResponse() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users?delay=3")
+                .then()
+                .statusCode(200)
+                .body("total", is(12))
+                .body("data.email[0]", is("george.bluth@reqres.in"));
+    }
+
+    @Test
+    void createUser() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void listUsers() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("total", is(12))
+                .body("data.email[0]", is("michael.lawson@reqres.in"));
+    }
+
+    @Test
+    void singleUsers() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+
+    @Test
+    void singleUserNotFound() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void singleResourceNotFound() {
+        given()
+                .when()
+                .get("https://reqres.in/api/inknown/23")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void listResource() {
+        given()
+                .when()
+                .get("https://reqres.in/api/unknown")
+                .then()
+                .statusCode(200)
+                .body("data.id[0]", is(1))
+                .body("data.name[0]", is("cerulean"))
+                .body("data.name[0]", is("cerulean"))
+                .body("data.year[0]", is(2000))
+                .body("data.color[0]", is("#98B2D1"))
+                .body("data.pantone_value[0]", is("15-4020"));
+    }
+
+    @Test
+    void updateUser() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void updatePatchUser() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .patch("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("name", is("morpheus"))
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void deleteUser() {
+
+        given()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    void listResourceWithAssertThat() {
+        Integer response =
+                given()
+                        .when()
+                        .get("https://reqres.in/api/unknown")
+                        .then()
+                        .statusCode(200)
+                        .extract().path("total");
+
+        assertThat(response).isEqualTo(12);
+    }
+
+
+}
